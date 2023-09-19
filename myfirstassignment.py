34b67067f43b0a8041d00d6632108f11dec726b1
sruthi1603 <145433343+sruthi1603@users.noreply.github.com>
2023-09-19 13:22:05 +0530

    Create myfirstassignment

---
+++ myfirstassignment.py
@@ -0,0 +1,20 @@
+Name=str(input("Name of a Student: "))
+sub1=int(input("Maths: "))
+sub2=int(input("Physics: "))
+sub3=int(input("Chemistry: "))
+sub4=int(input("Biology: "))
+sub5=int(input("English: "))
+Total=sub1+sub2+sub3+sub4+sub5
+print("Total Score: ",Total)
+percentage=(Total/500)*100
+print("Percentage:",percentage,"%")
+# percentage=int(input("Enter Percentage:"))
+if percentage>=80 and percentage<=100:
+    print("The Grade is A")
+elif percentage>=60 and percentage<80:
+    print("The Grade is B")
+elif percentage>=35 and percentage<60:
+    print("The Grade is C")
+else:
+    print("Fail")
+    
